cedb6dfd614e39f2dc018eeadf9a29280c501bdf
Ragul171 <rbragul07@gmail.com>
2022-10-11 21:56:27 +0530

    updated files added

--- predict.js
+++ predict.js
@@ -1,3 +1,11 @@
-window.onload = ()=>{
-    console.log("Hello")
+window.onload=function(){
+    document.getElementById('btn').addEventListener("click", function() {
+        document.getElementById('file').click();
+    });
+    
 }
+
+var loadFile = function(event) {
+    var image = document.getElementById('output');
+    image.src = URL.createObjectURL(event.target.files[0]);
+};
